0a69d32649bdf32a0c1368b38a718352d323e1e5
Oscar Phillip Springsteen <pspringsteen160@gmail.com>
2020-08-27 08:44:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board=["X", "X", "X", " ", " ", " ", " ", " ", " "])
+  r="-----------"
+  #{colors[0]}
+  puts " O | O | O "
+  puts "----------
+  puts
+  puts r
+  puts
+end
